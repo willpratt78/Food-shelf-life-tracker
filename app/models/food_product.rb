7929c64d105b3864da9f964c8ece89fa467667f6
William Pratt <willpratt78@gmail.com>
2022-07-15 14:11:55 -0700

    validations for food product

--- app/models/food_product.rb
+++ app/models/food_product.rb
@@ -1,2 +1,10 @@
 class FoodProduct < ApplicationRecord
+  validates :food_, presence :true
+  validates :amount_ordered_, presence :true
+  validates :amount_to_sell_, presence :true
+  validates :amount_of_pans_, presence :true
+  validates :date_ordered_, presence :true
+  validates :date_order_arrives_, presence :true
+  validates :soft_out_date_, presence :true
+  validates :hard_out_date_, presence :true
 end
