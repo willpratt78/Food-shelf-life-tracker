cba485cf36b2c8b7fc20fb6d24ae00b80553f9d5
William Pratt <willpratt78@gmail.com>
2022-07-24 19:01:52 -0700

    add decrement_to_sell and decrement_amount_o_pans

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,12 @@
 class Sale < ApplicationRecord
   belongs_to :food_product
   belongs_to :menu_item
+
+  def decrement_to_sell
+    self.food_product.update_attribute("amount_ordered_",(food_product.amount_ordered_ - 1))
+  end
+
+  def decrement_amount_of_pans
+    self.food_product.update_attribute("amount_of_pans",(food_product.amount_ordered_ / food_product.orders_per_pan_))
+  end
 end
