304eed4c369cf634461035998e43614943fc97b7
William Pratt <willpratt78@gmail.com>
2022-07-26 16:49:41 -0700

    still playing in the counsole to get amount_to_sell_ to decrement

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,5 +1,5 @@
 class MenuItem < ApplicationRecord
   has_many :sales
   has_many :food_products, :through => :sales
-
+  accepts_nested_attributes_for :sales, :food_products
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -11,3 +11,4 @@ class Sale < ApplicationRecord
   end
 
 end
+
