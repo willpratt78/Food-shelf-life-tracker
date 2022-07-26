135cb57429c517582710835de718b3e809b60b90
William Pratt <willpratt78@gmail.com>
2022-07-26 12:33:14 -0700

    working on pathing with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'home#index'
+    if current_user
+      root to: 'menu_items#index'
+    else
+      devise_scope :user do
+        root to: 'devise/registrations#new'
+      end
+    end
   resources :food_products
   resources :menu_items
 end
