684878843cabaef772d8c373377bcc5b951fc314
William Pratt <willpratt78@gmail.com>
2022-07-26 21:03:08 -0700

    update menu_items_controller & models

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -50,6 +50,16 @@ class MenuItemsController < ApplicationController
     redirect_to menu_items_path
   end
 
+  def decrement_to_sell
+    @menu_item = MenuItem.find(params[:id])
+    @menu_item.decrement_to_sell
+  end
+
+  def decrement_amount_of_pans
+    @menu_item = MenuItem.find(params[:id])
+    @menu_item.decrement_amount_of_pans
+  end
+
   private
     def menu_item_params
       params.require(:menu_item).permit(:name_, :food_item1_, :food_item2_, :food_item3_, :food_products_ids [], :food_products_food)
