1c9294f998f9fd083842c78f7b2837f1b5fe86a0
William Pratt <willpratt78@gmail.com>
2022-07-27 10:25:54 -0700

    Postgres broke

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -2,4 +2,13 @@ class MenuItem < ApplicationRecord
   has_many :sales
   has_many :food_products, :through => :sales
   accepts_nested_attributes_for :sales, :food_products
+
+  def decrement_to_sell
+    self.food_products.all[0].update_columns(amount_to_sell_: (self.food_products.all[0].amount_to_sell_ - 1))
+  end
+
+  def decrement_amount_of_pans
+    self.food_products.all[0].update_columns(amount_of_pans_: (self.food_products.all[0].amount_to_sell_ / self.food_products.all[0].orders_per_pan_.to_f).ceil)
+  end
+  
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -2,14 +2,6 @@ class Sale < ApplicationRecord
   belongs_to :food_product
   belongs_to :menu_item
 
-  def decrement_to_sell
-    self.food_products.all[0].update_columns(amount_to_sell_: (self.food_products.all[0].amount_to_sell_ - 1))
-  end
-
-  def decrement_amount_of_pans
-    self.food_products.all[0].update_columns(amount_of_pans_: (self.food_products.all[0].amount_to_sell_ / self.food_products.all[0].orders_per_pan_.to_f).ceil)
-  end
-
 end
 
 
