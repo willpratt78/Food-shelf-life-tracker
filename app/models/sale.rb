a92438721e88934ea8a29241e062f0736a71b36a
William Pratt <willpratt78@gmail.com>
2022-07-26 19:56:36 -0700

    update decrement_amount_of_pans

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -3,11 +3,11 @@ class Sale < ApplicationRecord
   belongs_to :menu_item
 
   def decrement_to_sell
-    self.food_products.all[0].update_columns(amount_to_sell_: (t1.food_products.all[0].amount_to_sell_ - 1))
+    self.food_products.all[0].update_columns(amount_to_sell_: (self.food_products.all[0].amount_to_sell_ - 1))
   end
 
   def decrement_amount_of_pans
-    self.food_product.update_attribute("amount_of_pans",(food_product.amount_ordered_ / food_product.orders_per_pan_))
+    self.food_product.all[0].update_columns(amount_of_pans: (self.food_product.all[0].amount_to_sell_ / self.food_product.all[0].orders_per_pan_.to_f).ceil)
   end
 
 end
